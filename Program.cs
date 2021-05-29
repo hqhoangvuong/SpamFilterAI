29cb0ab58521f3eda7523f53f7ba31a82e8eddd1
hqhoangvuong <hoangvuong19991964@gmail.com>
2021-05-29 11:18:36 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubSpamCommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
