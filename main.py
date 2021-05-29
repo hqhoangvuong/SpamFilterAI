be10b629353564da537c641a4f6ee25c564ca1e7
hqhoangvuong <hoangvuong19991964@gmail.com>
2021-05-29 11:37:05 +0000

    Added 15 GUID

---
+++ main.py
@@ -0,0 +1,20 @@
+import uuid
+import random as rng
+import subprocess
+
+my_uuid = uuid.uuid4()
+commit_count = 0
+while(True):
+    added_lines = ''
+    num_of_added_line = rng.randint(1, 20)
+    sleep_time = rng.randint(1000, 9000)
+    for idx in range(num_of_added_line):
+        added_lines += str(my_uuid) + '\n'
+    with open("guid_lib.txt", "a") as f:
+        f.write(added_lines)
+        
+    Process = subprocess.check_call(['./script/commit-push.sh', 'Added ' + str(num_of_added_line) + ' GUID'])
+    print('Commit No. {0}, sleep for {1}s'.format(commit_count, str(sleep_time/1000)))
+    commit_count += 1
+    
+    
